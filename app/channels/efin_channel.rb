146a489655920cae6e0d2e0b7031c2694b891e04
Yechiel Kalmenson <contact@yechiel.me>
2017-12-31 20:48:00 -0500

    Configure Rails to recieve data from websocket and return a response

--- app/channels/efin_channel.rb
+++ app/channels/efin_channel.rb
@@ -1,9 +1,10 @@
 require 'securerandom'
 class EfinChannel < ApplicationCable::Channel
   def subscribed
-    stream_from ""
+    stream_from "EfinChannel"
   end
   def receive(data)
+    ActionCable.server.broadcast('EfinChannel', sent_by: "Yechiel", body: "Hello World")
   end
 
   def unsubscribed
