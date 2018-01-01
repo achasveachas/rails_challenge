eaededd1f6a9ffe2bfadedc93b603934d539b74f
Yechiel Kalmenson <contact@yechiel.me>
2017-12-31 23:32:00 -0500

    add some validations on the data being submitted

--- app/jobs/efin_job.rb
+++ app/jobs/efin_job.rb
@@ -2,24 +2,28 @@ class EfinJob < ApplicationJob
   queue_as :default
 
   def perform(data)
-    ActionCable.server.broadcast('EfinChannel', body: "Hello World")
-    response = post(data)
-    ActionCable.server.broadcast('EfinChannel', body: response)
+    if data['household'].to_i > 0 && data['income'].to_i > 0
+      ActionCable.server.broadcast('EfinChannel', body: "Retrieving...")
+      response = post(data)
+      ActionCable.server.broadcast('EfinChannel', body: response)
+    else
+      ActionCable.server.broadcast('EfinChannel', body: "Sorry, an error has occured")
+    end      
   end
 
   private
 
   def post(data)
     payload = {
-      household: data['household'].to_i,
-      income: data['income'].to_i
+      household: data['household'],
+      income: data['income']
     }
 
     
     response = Faraday.post do |req|
       req.url 'http://efin.oddball.io'
       req.headers['Content-Type'] = 'application/json'
-      req.body = payload.to_json
+      req.body = data.to_json
     end
     parseXML(response.body)
   end
@@ -29,11 +33,4 @@ class EfinJob < ApplicationJob
     efin = parsed.xpath('//root/efin')[0].content
   end
 
-  def api
-    Faraday.new do |faraday|
-      faraday.response :logger
-      faraday.adapter Faraday.default_adapter
-      faraday.headers['Content-Type'] = 'application/xml'
-    end
-  end
 end
